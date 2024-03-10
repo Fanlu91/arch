0cbf1e3b5b3e3da3af1bb083e79f4ac48157d24a
fanlu91 <fanlu91@gmail.com>
2024-03-10 20:48:41 +0800

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# arch
+
+# idea 环境问题处理
+Enable Annotation Processing: 在 IntelliJ IDEA 中，确保启用了注解处理。你可以在 Settings 或 Preferences -> Build, Execution, Deployment -> Compiler -> Annotation Processors 中找到这个选项。
---
+++ myrpc-core/src/test/java/com/flhai/myrpc/core/MyrpcCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flhai.myrpc.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrpcCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
