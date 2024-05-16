3ba4d34bb9d6e032fcec32b486909df78fd7e15b
fanlu91 <fanlu91@gmail.com>
2024-05-16 18:53:05 +0800

    feat: add myrc provider renew

--- myrpc-core/src/main/java/com/flhai/myrpc/core/registry/my/MyRegistryCenter.java
+++ myrpc-core/src/main/java/com/flhai/myrpc/core/registry/my/MyRegistryCenter.java
@@ -9,6 +9,8 @@ import com.flhai.myrpc.core.registry.Event;
 import com.flhai.myrpc.core.registry.RegistryCenter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import java.util.HashMap;
 import java.util.List;
@@ -16,6 +18,7 @@ import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class MyRegistryCenter implements RegistryCenter {
@@ -24,22 +27,46 @@ public class MyRegistryCenter implements RegistryCenter {
     private String servers;
 
     Map<String, Long> VERSIONS = new HashMap<>();
-    ScheduledExecutorService scheduledExecutorService = null;
+    MultiValueMap<InstanceMeta, ServiceMeta> RENEWS = new LinkedMultiValueMap<>();
+    ScheduledExecutorService consumerExecutorService = null;
+    ScheduledExecutorService providerExecutorService = null;
 
     @Override
     public void start() {
         log.info("==> start my registry client with servers: {}", servers);
-        scheduledExecutorService = Executors.newScheduledThreadPool(1);
+        consumerExecutorService = Executors.newScheduledThreadPool(1);
+        providerExecutorService = Executors.newScheduledThreadPool(1);
+
+        providerExecutorService.scheduleAtFixedRate(() -> {
+            log.debug("==> renew all instances");
+            RENEWS.forEach((instance , serviceNames) -> {
+                log.debug("==> renew instance: {} for {} services", instance.toUrl(), serviceNames.size());
+                try {
+                    String servicesAsString = String.join((CharSequence) ",", serviceNames.stream().map(x -> x.toPath()).collect(Collectors.toList()));
+                    if (servicesAsString.endsWith(",")) {
+                        servicesAsString = servicesAsString.substring(0, servicesAsString.length() - 1);
+                    }
+                    HttpInvoker.httpPost(instance.toMetaString(), servers + "/renews?services=" + servicesAsString, String.class);
+                } catch (Exception e) {
+                    log.error("==> renew instance: {} for {} services failed", instance.toUrl(), serviceNames.size(), e);
+                }
+            });
+        }, 5, 5, TimeUnit.SECONDS);
     }
 
     @Override
     public void stop() {
         log.info("==> stop my registry client with servers: {}", servers);
-        scheduledExecutorService.shutdown();
+        gracefulShutdown(consumerExecutorService);
+        gracefulShutdown(providerExecutorService);
+    }
+
+    private void gracefulShutdown(ScheduledExecutorService executorService) {
+        executorService.shutdown();
         try {
-            scheduledExecutorService.awaitTermination(1, TimeUnit.SECONDS);
-            if (!scheduledExecutorService.isTerminated()) {
-                scheduledExecutorService.shutdownNow();
+            executorService.awaitTermination(1, TimeUnit.SECONDS);
+            if (!executorService.isTerminated()) {
+                executorService.shutdownNow();
             }
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
@@ -50,12 +77,14 @@ public class MyRegistryCenter implements RegistryCenter {
     public void register(ServiceMeta serviceName, InstanceMeta instance) {
         log.info("==> register service: {} instance: {}", serviceName.toPath(), instance);
         HttpInvoker.httpPost(instance.toMetaString(), servers + "/register?service=" + serviceName.toPath(), InstanceMeta.class);
+        RENEWS.add(instance, serviceName);
     }
 
     @Override
     public void unregister(ServiceMeta serviceName, InstanceMeta instance) {
         log.info("==> unregister service: {} instance: {}", serviceName, instance);
         HttpInvoker.httpPost(instance.toMetaString(), servers + "/unregister?service=" + serviceName.toPath(), InstanceMeta.class);
+        RENEWS.remove(instance, serviceName);
     }
 
     @Override
@@ -67,11 +96,10 @@ public class MyRegistryCenter implements RegistryCenter {
         return instanceMetaList;
     }
 
-
     @Override
     public void subscribe(ServiceMeta serviceMeta, ChangedListener listener) {
         log.info("==> subscribe service: {}", serviceMeta.toPath());
-        scheduledExecutorService.scheduleAtFixedRate(() -> {
+        consumerExecutorService.scheduleAtFixedRate(() -> {
             Long version = VERSIONS.getOrDefault(serviceMeta.toPath(), -1L);
             Long remoteVersion = HttpInvoker.httpGet(servers + "/version?service=" + serviceMeta.toPath(), Long.class);
             if (remoteVersion > version) {
